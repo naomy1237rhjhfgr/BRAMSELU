0f964cbb795802ed8fba672760631a0ed50c1401
naomy1237rhjhfgr <lagosb813@gmail.com>
2026-06-24 16:24:03 -0600

    cambios

--- Empleado.cs
+++ Empleado.cs
@@ -1,22 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BRAMSELU
 {
     internal class Empleado
     {
-        public int IdEmpleado { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string Identidad { get; set; }
-        public string Telefono { get; set; }
-        public string Direccion { get; set; }
-        public string Correo { get; set; }
-        public string Usuario { get; set; }
-        public string Contrasena { get; set; }
-        public string TipoUsuario { get; set; }
+       
+        private int idEmpleado;
+        private string nombre;
+        private string apellido;
+        private string identidad;
+        private string telefono;
+        private string direccion;
+        private string correo;
+        private string usuario;
+        private string contrasena;
+        private string tipoUsuario;
+
+       
+
+        public int IdEmpleado
+        {
+            get { return idEmpleado; }
+            set { idEmpleado = value; }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        public string Identidad
+        {
+            get { return identidad; }
+            set { identidad = value; }
+        }
+
+        public string Telefono
+        {
+            get { return telefono; }
+            set { telefono = value; }
+        }
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        public string Correo
+        {
+            get { return correo; }
+            set { correo = value; }
+        }
+
+        public string Usuario
+        {
+            get { return usuario; }
+            set { usuario = value; }
+        }
+
+        public string Contrasena
+        {
+            get { return contrasena; }
+            set { contrasena = value; }
+        }
+
+        public string TipoUsuario
+        {
+            get { return tipoUsuario; }
+            set { tipoUsuario = value; }
+        }
     }
 }
