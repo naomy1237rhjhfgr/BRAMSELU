9440c8fd9f73e82eb5f307ab2371e47badfb42e9
Carmen Andrea Martínez Reyes <cara_martinezr00@unicah.edu>
2026-07-01 20:15:41 -0600

    hjhjcdb

---
+++ ClaseCitas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRAMSELU
+{
+    public class ClaseCitas
+    {
+        private string _cliente;
+        private int _telefono;
+        private string _servicio;
+        private string _especialista;
+        private string _notas;
+        private string _estado;
+        private string _precio;
+
+        private ClaseCitas()
+        {
+            _cliente = string.Empty;
+            _telefono = 0;
+            _servicio = "";
+        }
+    }
+}
