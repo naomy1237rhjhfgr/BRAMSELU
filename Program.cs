d10824e4f4f5c52f43c52838447a92d36ba8e830
Carmen Andrea Martínez Reyes <cara_martinezr00@unicah.edu>
2026-07-12 12:26:50 -0600

    .

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace BRAMSELU
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmClientes());
+            Application.Run(new frmLogin());
         }
     }
 }
