66ad2267e59400a5fe27539b24d5fc0cfbdf89b8
naomy1237rhjhfgr <lagosb813@gmail.com>
2026-07-12 21:58:44 -0600

    kjfd

--- CLasesClientes/Cliente.cs
+++ CLasesClientes/Cliente.cs
@@ -1,26 +1,65 @@
-﻿using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
-namespace BRAMSELU
+namespace BRAMSELU.Entidades
 {
     public class Cliente
     {
+        private string _id;
+        private string _nombre;
+        private string _telefono;
+        private string _correo;
+        private string _direccion;
+        private string _tipoPiel;
 
-        public string Id { get; set; }
-        public string Nombre { get; set; }
-        public string Telefono { get; set; }
-        public string Correo { get; set; }
-        public string Direccion { get; set; }
-        public string TipoPiel { get; set; }
-
-        public Cliente(string id, string nombre, string telefono, string correo, string direccion, string tipoPiel)
+        public Cliente()
         {
-            Id = id;
-            Nombre = nombre;
-            Telefono = telefono;
-            Correo = correo;
-            Direccion = direccion;
-            TipoPiel = tipoPiel;
+            _id = "";
+
+            _nombre = "";
+
+            _telefono = "";
+
+            _correo = "";
+
+            _direccion = "";
+
+            _tipoPiel = "";
+        }
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public string Telefono
+        {
+            get { return _telefono; }
+            set { _telefono = value; }
+        }
+
+        public string Correo
+        {
+            get { return _correo; }
+            set { _correo = value; }
+        }
+
+        public string Direccion
+        {
+            get { return _direccion; }
+            set { _direccion = value; }
+        }
+
+        public string TipoPiel
+        {
+            get { return _tipoPiel; }
+            set { _tipoPiel = value; }
         }
     }
 }
