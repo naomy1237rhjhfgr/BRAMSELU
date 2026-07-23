a76b14f6f304b2d370945cd86444fd790840c97c
ANTONIOMEJIA2004 <mejiaabdiel24@gmail.com>
2026-07-22 20:38:33 -0600

    Avanse de CRUD de catgeoria, faltan varias cosas aun

---
+++ Categorias/CategoriaDatos.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BRAMSELU.Categorias
+{
+    public class CategoriaDatos
+    {
+        private Conexion conexion = new Conexion();
+
+        public DataTable ObtenerCategorias()
+        {
+            string sql = " SELECT * FROM Categorias";
+
+            return conexion.EjecutarConsultaDataTable(sql);
+        }
+
+        public bool GuardarCategoria(Categoria categoria)
+        {
+            string sql = $"INSERT INTO Categorias (NombreCategoria, Descripcion)" +
+                            $"VALUES ('{categoria.NombreCategoria}','{categoria.Descripcion}')";
+
+            return conexion.EjecutarSQL(sql);
+        }
+
+        public bool ActualizarCategoria(Categoria categoria)
+        {
+            string sql = $"UPDATE Categorias SET " +
+                        $"NombreCategoria = '{categoria.NombreCategoria}'," +
+                        $"Descripcion = '{categoria.Descripcion}' " +
+                        $"WHERE IdCategoria = {categoria.IdCategoria}";
+
+            return conexion.EjecutarSQL(sql);
+        }
+
+        public bool EliminarCategoria(int idCategoria)
+        {
+            string sql = $"DELETE FROM Categorias WHERE IdCategoria = {idCategoria}";
+
+            return conexion.EjecutarSQL(sql);  
+        }
+
+        public DataTable BuscarCategoria(string nombre)
+        {
+            string sql = $"SELECT * FROM Categorias WHERE NombreCategoria LIKE '%{nombre}%'";
+
+            return conexion.EjecutarConsultaDataTable(sql);
+        }
+
+        public bool ExisteCategoria(string nombreCategoria, int idCategoria)
+        {
+            string sql = $"SELECT * FROM Categorias " +
+                         $"WHERE NombreCategoria = '{nombreCategoria}' " +
+                         $"AND IdCategoria <> {idCategoria}";
+
+            SqlDataReader reader = conexion.EjecutarConsultaUno(sql);
+
+            if (reader != null)
+            {
+                reader.Close();
+                conexion.Cerrar();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Categorias/frmCategorias.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace BRAMSELU
+{
+    partial class frmCategorias
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblcategorias = new System.Windows.Forms.Label();
+            this.progressBarclientes = new System.Windows.Forms.ProgressBar();
+            this.txtidcategoria = new System.Windows.Forms.MaskedTextBox();
+            this.lblidcategoria = new System.Windows.Forms.Label();
+            this.labeldescripcion = new System.Windows.Forms.Label();
+            this.txtnombrecategoria = new System.Windows.Forms.TextBox();
+            this.labelnombrecategoria = new System.Windows.Forms.Label();
+            this.lbldatoscategoria = new System.Windows.Forms.Label();
+            this.btnbuscarcategoria = new System.Windows.Forms.Button();
+            this.txtBuscarcategoria = new System.Windows.Forms.TextBox();
+            this.btnguardarcategoria = new System.Windows.Forms.Button();
+            this.bttneliminarcategoria = new System.Windows.Forms.Button();
+            this.bttneditarcategoria = new System.Windows.Forms.Button();
+            this.Btnnuevocategoria = new System.Windows.Forms.Button();
+            this.dataGridViewcategoria = new System.Windows.Forms.DataGridView();
+            this.labelbuscarcategoria = new System.Windows.Forms.Label();
+            this.txtdescripcion = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewcategoria)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblcategorias
+            // 
+            this.lblcategorias.AutoSize = true;
+            this.lblcategorias.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblcategorias.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(110)))), ((int)(((byte)(75)))));
+            this.lblcategorias.Location = new System.Drawing.Point(12, 9);
+            this.lblcategorias.Name = "lblcategorias";
+            this.lblcategorias.Size = new System.Drawing.Size(155, 37);
+            this.lblcategorias.TabIndex = 13;
+            this.lblcategorias.Text = "Categorias";
+            // 
+            // progressBarclientes
+            // 
+            this.progressBarclientes.Location = new System.Drawing.Point(27, 561);
+            this.progressBarclientes.Name = "progressBarclientes";
+            this.progressBarclientes.Size = new System.Drawing.Size(129, 23);
+            this.progressBarclientes.TabIndex = 62;
+            this.progressBarclientes.Visible = false;
+            // 
+            // txtidcategoria
+            // 
+            this.txtidcategoria.Location = new System.Drawing.Point(138, 319);
+            this.txtidcategoria.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtidcategoria.Name = "txtidcategoria";
+            this.txtidcategoria.Size = new System.Drawing.Size(340, 22);
+            this.txtidcategoria.TabIndex = 59;
+            // 
+            // lblidcategoria
+            // 
+            this.lblidcategoria.AutoSize = true;
+            this.lblidcategoria.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.lblidcategoria.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(75)))), ((int)(((byte)(65)))));
+            this.lblidcategoria.Location = new System.Drawing.Point(23, 318);
+            this.lblidcategoria.Name = "lblidcategoria";
+            this.lblidcategoria.Size = new System.Drawing.Size(94, 21);
+            this.lblidcategoria.TabIndex = 58;
+            this.lblidcategoria.Text = "Id Categoria";
+            // 
+            // labeldescripcion
+            // 
+            this.labeldescripcion.AutoSize = true;
+            this.labeldescripcion.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.labeldescripcion.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(75)))), ((int)(((byte)(65)))));
+            this.labeldescripcion.Location = new System.Drawing.Point(23, 396);
+            this.labeldescripcion.Name = "labeldescripcion";
+            this.labeldescripcion.Size = new System.Drawing.Size(94, 21);
+            this.labeldescripcion.TabIndex = 52;
+            this.labeldescripcion.Text = "Descripcion:";
+            // 
+            // txtnombrecategoria
+            // 
+            this.txtnombrecategoria.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtnombrecategoria.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtnombrecategoria.Location = new System.Drawing.Point(138, 352);
+            this.txtnombrecategoria.Name = "txtnombrecategoria";
+            this.txtnombrecategoria.Size = new System.Drawing.Size(340, 30);
+            this.txtnombrecategoria.TabIndex = 51;
+            // 
+            // labelnombrecategoria
+            // 
+            this.labelnombrecategoria.AutoSize = true;
+            this.labelnombrecategoria.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.labelnombrecategoria.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(75)))), ((int)(((byte)(65)))));
+            this.labelnombrecategoria.Location = new System.Drawing.Point(23, 355);
+            this.labelnombrecategoria.Name = "labelnombrecategoria";
+            this.labelnombrecategoria.Size = new System.Drawing.Size(71, 21);
+            this.labelnombrecategoria.TabIndex = 50;
+            this.labelnombrecategoria.Text = "Nombre:";
+            // 
+            // lbldatoscategoria
+            // 
+            this.lbldatoscategoria.AutoSize = true;
+            this.lbldatoscategoria.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Bold);
+            this.lbldatoscategoria.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(110)))), ((int)(((byte)(75)))));
+            this.lbldatoscategoria.Location = new System.Drawing.Point(23, 284);
+            this.lbldatoscategoria.Name = "lbldatoscategoria";
+            this.lbldatoscategoria.Size = new System.Drawing.Size(173, 21);
+            this.lbldatoscategoria.TabIndex = 49;
+            this.lbldatoscategoria.Text = "Datos de la Categoria";
+            // 
+            // btnbuscarcategoria
+            // 
+            this.btnbuscarcategoria.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(130)))), ((int)(((byte)(160)))));
+            this.btnbuscarcategoria.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnbuscarcategoria.FlatAppearance.BorderSize = 0;
+            this.btnbuscarcategoria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnbuscarcategoria.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnbuscarcategoria.ForeColor = System.Drawing.Color.White;
+            this.btnbuscarcategoria.Location = new System.Drawing.Point(306, 70);
+            this.btnbuscarcategoria.Name = "btnbuscarcategoria";
+            this.btnbuscarcategoria.Size = new System.Drawing.Size(100, 32);
+            this.btnbuscarcategoria.TabIndex = 48;
+            this.btnbuscarcategoria.Text = "Buscar";
+            this.btnbuscarcategoria.UseVisualStyleBackColor = false;
+            this.btnbuscarcategoria.Click += new System.EventHandler(this.btnbuscarcategoria_Click);
+            // 
+            // txtBuscarcategoria
+            // 
+            this.txtBuscarcategoria.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBuscarcategoria.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.txtBuscarcategoria.Location = new System.Drawing.Point(89, 70);
+            this.txtBuscarcategoria.Name = "txtBuscarcategoria";
+            this.txtBuscarcategoria.Size = new System.Drawing.Size(195, 30);
+            this.txtBuscarcategoria.TabIndex = 47;
+            // 
+            // btnguardarcategoria
+            // 
+            this.btnguardarcategoria.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(120)))), ((int)(((byte)(150)))), ((int)(((byte)(110)))));
+            this.btnguardarcategoria.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnguardarcategoria.FlatAppearance.BorderSize = 0;
+            this.btnguardarcategoria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnguardarcategoria.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnguardarcategoria.ForeColor = System.Drawing.Color.White;
+            this.btnguardarcategoria.Location = new System.Drawing.Point(780, 70);
+            this.btnguardarcategoria.Name = "btnguardarcategoria";
+            this.btnguardarcategoria.Size = new System.Drawing.Size(100, 32);
+            this.btnguardarcategoria.TabIndex = 46;
+            this.btnguardarcategoria.Text = "Guardar";
+            this.btnguardarcategoria.UseVisualStyleBackColor = false;
+            this.btnguardarcategoria.Click += new System.EventHandler(this.btnguardarcategoria_Click);
+            // 
+            // bttneliminarcategoria
+            // 
+            this.bttneliminarcategoria.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(90)))), ((int)(((byte)(80)))));
+            this.bttneliminarcategoria.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttneliminarcategoria.FlatAppearance.BorderSize = 0;
+            this.bttneliminarcategoria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttneliminarcategoria.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.bttneliminarcategoria.ForeColor = System.Drawing.Color.White;
+            this.bttneliminarcategoria.Location = new System.Drawing.Point(665, 70);
+            this.bttneliminarcategoria.Name = "bttneliminarcategoria";
+            this.bttneliminarcategoria.Size = new System.Drawing.Size(100, 32);
+            this.bttneliminarcategoria.TabIndex = 45;
+            this.bttneliminarcategoria.Text = "Eliminar";
+            this.bttneliminarcategoria.UseVisualStyleBackColor = false;
+            this.bttneliminarcategoria.Click += new System.EventHandler(this.bttneliminarcategoria_Click);
+            // 
+            // bttneditarcategoria
+            // 
+            this.bttneditarcategoria.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(110)))), ((int)(((byte)(75)))));
+            this.bttneditarcategoria.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttneditarcategoria.FlatAppearance.BorderSize = 0;
+            this.bttneditarcategoria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttneditarcategoria.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.bttneditarcategoria.ForeColor = System.Drawing.Color.White;
+            this.bttneditarcategoria.Location = new System.Drawing.Point(548, 70);
+            this.bttneditarcategoria.Name = "bttneditarcategoria";
+            this.bttneditarcategoria.Size = new System.Drawing.Size(100, 32);
+            this.bttneditarcategoria.TabIndex = 44;
+            this.bttneditarcategoria.Text = "Editar";
+            this.bttneditarcategoria.UseVisualStyleBackColor = false;
+            this.bttneditarcategoria.Click += new System.EventHandler(this.bttneditarcategoria_Click);
+            // 
+            // Btnnuevocategoria
+            // 
+            this.Btnnuevocategoria.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(154)))), ((int)(((byte)(108)))));
+            this.Btnnuevocategoria.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btnnuevocategoria.FlatAppearance.BorderSize = 0;
+            this.Btnnuevocategoria.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btnnuevocategoria.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.Btnnuevocategoria.ForeColor = System.Drawing.Color.White;
+            this.Btnnuevocategoria.Location = new System.Drawing.Point(421, 70);
+            this.Btnnuevocategoria.Name = "Btnnuevocategoria";
+            this.Btnnuevocategoria.Size = new System.Drawing.Size(110, 32);
+            this.Btnnuevocategoria.TabIndex = 43;
+            this.Btnnuevocategoria.Text = "Nuevo";
+            this.Btnnuevocategoria.UseVisualStyleBackColor = false;
+            this.Btnnuevocategoria.Click += new System.EventHandler(this.Btnnuevocategoria_Click);
+            // 
+            // dataGridViewcategoria
+            // 
+            this.dataGridViewcategoria.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewcategoria.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewcategoria.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewcategoria.Location = new System.Drawing.Point(19, 121);
+            this.dataGridViewcategoria.Name = "dataGridViewcategoria";
+            this.dataGridViewcategoria.RowHeadersVisible = false;
+            this.dataGridViewcategoria.RowHeadersWidth = 51;
+            this.dataGridViewcategoria.RowTemplate.Height = 24;
+            this.dataGridViewcategoria.Size = new System.Drawing.Size(919, 146);
+            this.dataGridViewcategoria.TabIndex = 42;
+            // 
+            // labelbuscarcategoria
+            // 
+            this.labelbuscarcategoria.AutoSize = true;
+            this.labelbuscarcategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelbuscarcategoria.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(90)))), ((int)(((byte)(75)))), ((int)(((byte)(65)))));
+            this.labelbuscarcategoria.Location = new System.Drawing.Point(15, 74);
+            this.labelbuscarcategoria.Name = "labelbuscarcategoria";
+            this.labelbuscarcategoria.Size = new System.Drawing.Size(68, 20);
+            this.labelbuscarcategoria.TabIndex = 41;
+            this.labelbuscarcategoria.Text = "Buscar:";
+            // 
+            // txtdescripcion
+            // 
+            this.txtdescripcion.Location = new System.Drawing.Point(40, 431);
+            this.txtdescripcion.Multiline = true;
+            this.txtdescripcion.Name = "txtdescripcion";
+            this.txtdescripcion.Size = new System.Drawing.Size(438, 90);
+            this.txtdescripcion.TabIndex = 63;
+            // 
+            // frmCategorias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(237)))), ((int)(((byte)(230)))));
+            this.ClientSize = new System.Drawing.Size(979, 612);
+            this.Controls.Add(this.txtdescripcion);
+            this.Controls.Add(this.progressBarclientes);
+            this.Controls.Add(this.txtidcategoria);
+            this.Controls.Add(this.lblidcategoria);
+            this.Controls.Add(this.labeldescripcion);
+            this.Controls.Add(this.txtnombrecategoria);
+            this.Controls.Add(this.labelnombrecategoria);
+            this.Controls.Add(this.lbldatoscategoria);
+            this.Controls.Add(this.btnbuscarcategoria);
+            this.Controls.Add(this.txtBuscarcategoria);
+            this.Controls.Add(this.btnguardarcategoria);
+            this.Controls.Add(this.bttneliminarcategoria);
+            this.Controls.Add(this.bttneditarcategoria);
+            this.Controls.Add(this.Btnnuevocategoria);
+            this.Controls.Add(this.dataGridViewcategoria);
+            this.Controls.Add(this.labelbuscarcategoria);
+            this.Controls.Add(this.lblcategorias);
+            this.Name = "frmCategorias";
+            this.Text = "frmCategorias";
+            this.Load += new System.EventHandler(this.frmCategorias_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewcategoria)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblcategorias;
+        private System.Windows.Forms.ProgressBar progressBarclientes;
+        private System.Windows.Forms.MaskedTextBox txtidcategoria;
+        private System.Windows.Forms.Label lblidcategoria;
+        private System.Windows.Forms.Label labeldescripcion;
+        private System.Windows.Forms.TextBox txtnombrecategoria;
+        private System.Windows.Forms.Label labelnombrecategoria;
+        private System.Windows.Forms.Label lbldatoscategoria;
+        private System.Windows.Forms.Button btnbuscarcategoria;
+        private System.Windows.Forms.TextBox txtBuscarcategoria;
+        private System.Windows.Forms.Button btnguardarcategoria;
+        private System.Windows.Forms.Button bttneliminarcategoria;
+        private System.Windows.Forms.Button bttneditarcategoria;
+        private System.Windows.Forms.Button Btnnuevocategoria;
+        private System.Windows.Forms.DataGridView dataGridViewcategoria;
+        private System.Windows.Forms.Label labelbuscarcategoria;
+        private System.Windows.Forms.TextBox txtdescripcion;
+    }
+}
