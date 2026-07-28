9f80a1041de6fa8aa61fd6e12256bef7b21ddc35
Carmen Andrea Martínez Reyes <cara_martinezr00@unicah.edu>
2026-07-27 18:32:09 -0600

    nn.

--- otrosfrns/frmReportes.Designer.cs
+++ otrosfrns/frmReportes.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class frmReportes
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,15 +13,10 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.SuspendLayout();
+
             // 
             // frmReportes
             // 
@@ -40,7 +28,5 @@
             this.ResumeLayout(false);
 
         }
-
-        #endregion
     }
 }
