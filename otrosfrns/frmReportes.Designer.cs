cbb1ab39ff18a883d12ef9ea60b81ea724ebe02f
naomy1237rhjhfgr <lagosb813@gmail.com>
2026-07-30 16:50:35 -0600

    reportes

--- otrosfrns/frmReportes.Designer.cs
+++ otrosfrns/frmReportes.Designer.cs
@@ -15,43 +15,57 @@
 
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnventa = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.pnlContenedor = new System.Windows.Forms.Panel();
+            this.pnlContenedor.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // btnventa
             // 
-            this.button1.Location = new System.Drawing.Point(568, 123);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(147, 140);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnventa.Location = new System.Drawing.Point(506, 275);
+            this.btnventa.Name = "btnventa";
+            this.btnventa.Size = new System.Drawing.Size(147, 140);
+            this.btnventa.TabIndex = 0;
+            this.btnventa.Text = "button1";
+            this.btnventa.UseVisualStyleBackColor = true;
+            this.btnventa.Click += new System.EventHandler(this.btnventa_Click_1);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(278, 141);
+            this.button2.Location = new System.Drawing.Point(217, 293);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(147, 140);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // pnlContenedor
+            // 
+            this.pnlContenedor.Controls.Add(this.btnventa);
+            this.pnlContenedor.Controls.Add(this.button2);
+            this.pnlContenedor.Location = new System.Drawing.Point(37, 32);
+            this.pnlContenedor.Name = "pnlContenedor";
+            this.pnlContenedor.Size = new System.Drawing.Size(914, 436);
+            this.pnlContenedor.TabIndex = 2;
             // 
             // frmReportes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1028, 491);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pnlContenedor);
             this.Name = "frmReportes";
             this.Text = "frmReportes";
             this.Load += new System.EventHandler(this.frmReportes_Load);
+            this.pnlContenedor.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnventa;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel pnlContenedor;
     }
 }
--- otrosfrns/frmReportes.cs
+++ otrosfrns/frmReportes.cs
@@ -1,32 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using BRAMSELU.Ventas;
+
 namespace BRAMSELU
-
 {
-
     public partial class frmReportes : Form
-
     {
+        private Form formActivo = null;
 
         public frmReportes()
-
         {
-
             InitializeComponent();
-
         }
 
         private void frmReportes_Load(object sender, EventArgs e)
         {
-            // prueba
+
+        }
+
+        private void AbrirFormularioEnPanel(Form formHijo)
+        {
+            if (formActivo != null)
+            {
+                formActivo.Close();
+            }
+
+            formActivo = formHijo;
+            formHijo.TopLevel = false;
+            formHijo.FormBorderStyle = FormBorderStyle.None;
+            formHijo.Dock = DockStyle.Fill;
+
+            pnlContenedor.Controls.Add(formHijo);
+            pnlContenedor.Tag = formHijo;
+            formHijo.BringToFront();
+            formHijo.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frmReporteCompras frmCompras = new frmReporteCompras();
+            AbrirFormularioEnPanel(frmCompras);
+        }
+
+        private void btnventa_Click(object sender, EventArgs e)
+        {
+            FrmReporteVentas frmVentas = new FrmReporteVentas();
+            AbrirFormularioEnPanel(frmVentas);
+        }
+
+        private void btnventa_Click_1(object sender, EventArgs e)
+        {
+            FrmReporteVentas frmVentas = new FrmReporteVentas();
+            AbrirFormularioEnPanel(frmVentas);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            frmReporteCompras frmCompras = new frmReporteCompras();
+            AbrirFormularioEnPanel(frmCompras);
         }
     }
-
 }
