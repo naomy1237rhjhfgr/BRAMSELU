5f09db5b53b917b4309d4a279d0fba73e22df396
naomy1237rhjhfgr <lagosb813@gmail.com>
2026-07-30 16:19:46 -0600

    Agregué reporte

--- otrosfrns/frmReportes.Designer.cs
+++ otrosfrns/frmReportes.Designer.cs
@@ -15,18 +15,43 @@
 
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(568, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 140);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 140);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // frmReportes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1028, 491);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "frmReportes";
             this.Text = "frmReportes";
             this.Load += new System.EventHandler(this.frmReportes_Load);
             this.ResumeLayout(false);
 
         }
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
