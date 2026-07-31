f0ffcf70eff96b51dd3aace3c208777f22979a2a
naomy1237rhjhfgr <lagosb813@gmail.com>
2026-07-30 18:06:15 -0600

    compramnsadjhdhj jdf

--- Ventas/FrmVentas.Designer.cs
+++ Ventas/FrmVentas.Designer.cs
@@ -13,11 +13,10 @@
             base.Dispose(disposing);
         }
 
-
-
         private void InitializeComponent()
         {
-            this.cmbProductos = new System.Windows.Forms.ComboBox();
+            this.btnSeleccionarProducto = new System.Windows.Forms.Button();
+            this.txtProducto = new System.Windows.Forms.TextBox();
             this.txtCantidad = new System.Windows.Forms.TextBox();
             this.btnAgregar = new System.Windows.Forms.Button();
             this.dgvCarrito = new System.Windows.Forms.DataGridView();
@@ -31,31 +30,42 @@
             ((System.ComponentModel.ISupportInitialize)(this.dgvCarrito)).BeginInit();
             this.SuspendLayout();
             // 
-            // cmbProductos
+            // btnSeleccionarProducto
             // 
-            this.cmbProductos.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbProductos.FormattingEnabled = true;
-            this.cmbProductos.Location = new System.Drawing.Point(30, 45);
-            this.cmbProductos.Name = "cmbProductos";
-            this.cmbProductos.Size = new System.Drawing.Size(250, 28);
-            this.cmbProductos.TabIndex = 0;
+            this.btnSeleccionarProducto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnSeleccionarProducto.ForeColor = System.Drawing.Color.White;
+            this.btnSeleccionarProducto.Location = new System.Drawing.Point(30, 12);
+            this.btnSeleccionarProducto.Name = "btnSeleccionarProducto";
+            this.btnSeleccionarProducto.Size = new System.Drawing.Size(142, 68);
+            this.btnSeleccionarProducto.TabIndex = 0;
+            this.btnSeleccionarProducto.Text = "Seleccionar Producto";
+            this.btnSeleccionarProducto.UseVisualStyleBackColor = false;
+            this.btnSeleccionarProducto.Click += new System.EventHandler(this.btnSeleccionarProducto_Click);
+            // 
+            // txtProducto
+            // 
+            this.txtProducto.Enabled = false;
+            this.txtProducto.Location = new System.Drawing.Point(185, 45);
+            this.txtProducto.Name = "txtProducto";
+            this.txtProducto.Size = new System.Drawing.Size(180, 26);
+            this.txtProducto.TabIndex = 1;
             // 
             // txtCantidad
             // 
-            this.txtCantidad.Location = new System.Drawing.Point(300, 45);
+            this.txtCantidad.Location = new System.Drawing.Point(375, 45);
             this.txtCantidad.Name = "txtCantidad";
-            this.txtCantidad.Size = new System.Drawing.Size(100, 26);
-            this.txtCantidad.TabIndex = 1;
+            this.txtCantidad.Size = new System.Drawing.Size(80, 26);
+            this.txtCantidad.TabIndex = 2;
             // 
             // btnAgregar
             // 
             this.btnAgregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.btnAgregar.ForeColor = System.Drawing.Color.White;
-            this.btnAgregar.Location = new System.Drawing.Point(406, 36);
+            this.btnAgregar.Location = new System.Drawing.Point(465, 36);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(142, 44);
-            this.btnAgregar.TabIndex = 2;
-            this.btnAgregar.Text = "Agregar al Carrito";
+            this.btnAgregar.Size = new System.Drawing.Size(130, 44);
+            this.btnAgregar.TabIndex = 3;
+            this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = false;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
@@ -64,22 +74,22 @@
             this.dgvCarrito.AllowUserToAddRows = false;
             this.dgvCarrito.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvCarrito.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCarrito.Location = new System.Drawing.Point(30, 90);
+            this.dgvCarrito.Location = new System.Drawing.Point(30, 95);
             this.dgvCarrito.Name = "dgvCarrito";
             this.dgvCarrito.RowHeadersWidth = 51;
             this.dgvCarrito.RowTemplate.Height = 24;
-            this.dgvCarrito.Size = new System.Drawing.Size(500, 250);
-            this.dgvCarrito.TabIndex = 3;
+            this.dgvCarrito.Size = new System.Drawing.Size(565, 250);
+            this.dgvCarrito.TabIndex = 4;
             // 
             // lblTotal
             // 
             this.lblTotal.AutoSize = true;
             this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTotal.ForeColor = System.Drawing.Color.Green;
-            this.lblTotal.Location = new System.Drawing.Point(380, 360);
+            this.lblTotal.Location = new System.Drawing.Point(420, 360);
             this.lblTotal.Name = "lblTotal";
             this.lblTotal.Size = new System.Drawing.Size(108, 32);
-            this.lblTotal.TabIndex = 4;
+            this.lblTotal.TabIndex = 5;
             this.lblTotal.Text = "L. 0.00";
             // 
             // txtEfectivo
@@ -88,7 +98,7 @@
             this.txtEfectivo.Location = new System.Drawing.Point(180, 405);
             this.txtEfectivo.Name = "txtEfectivo";
             this.txtEfectivo.Size = new System.Drawing.Size(150, 35);
-            this.txtEfectivo.TabIndex = 5;
+            this.txtEfectivo.TabIndex = 6;
             this.txtEfectivo.TextChanged += new System.EventHandler(this.txtEfectivo_TextChanged);
             // 
             // btnCobrar
@@ -98,8 +108,8 @@
             this.btnCobrar.ForeColor = System.Drawing.Color.White;
             this.btnCobrar.Location = new System.Drawing.Point(350, 400);
             this.btnCobrar.Name = "btnCobrar";
-            this.btnCobrar.Size = new System.Drawing.Size(180, 40);
-            this.btnCobrar.TabIndex = 6;
+            this.btnCobrar.Size = new System.Drawing.Size(245, 40);
+            this.btnCobrar.TabIndex = 7;
             this.btnCobrar.Text = "Finalizar Cobro (Efectivo)";
             this.btnCobrar.UseVisualStyleBackColor = false;
             this.btnCobrar.Click += new System.EventHandler(this.btnCobrar_Click);
@@ -107,45 +117,45 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(30, 25);
+            this.label1.Location = new System.Drawing.Point(185, 20);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(159, 20);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Seleccione Producto:";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Producto:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(300, 25);
+            this.label2.Location = new System.Drawing.Point(375, 20);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(77, 20);
-            this.label2.TabIndex = 8;
+            this.label2.TabIndex = 9;
             this.label2.Text = "Cantidad:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(210, 360);
+            this.label3.Location = new System.Drawing.Point(240, 360);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(177, 29);
-            this.label3.TabIndex = 9;
+            this.label3.TabIndex = 10;
             this.label3.Text = "Total a Pagar:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(-11, 411);
+            this.label4.Location = new System.Drawing.Point(25, 411);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(185, 25);
-            this.label4.TabIndex = 10;
+            this.label4.TabIndex = 11;
             this.label4.Text = "Efectivo Recibido:";
             // 
             // FrmVentas
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(560, 470);
+            this.ClientSize = new System.Drawing.Size(630, 470);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -156,7 +166,8 @@
             this.Controls.Add(this.dgvCarrito);
             this.Controls.Add(this.btnAgregar);
             this.Controls.Add(this.txtCantidad);
-            this.Controls.Add(this.cmbProductos);
+            this.Controls.Add(this.txtProducto);
+            this.Controls.Add(this.btnSeleccionarProducto);
             this.Name = "FrmVentas";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Módulo de Ventas y Cobro Físico";
@@ -167,7 +178,8 @@
 
         }
 
-        private System.Windows.Forms.ComboBox cmbProductos;
+        private System.Windows.Forms.Button btnSeleccionarProducto;
+        private System.Windows.Forms.TextBox txtProducto;
         private System.Windows.Forms.TextBox txtCantidad;
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.DataGridView dgvCarrito;
