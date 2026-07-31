a11d16dec466630bbbf3c8a80e35510d7d5ca79a
naomy1237rhjhfgr <lagosb813@gmail.com>
2026-07-30 21:07:50 -0600

    jsjhdjhdhjd

---
+++ llamadoinventario/llamadoinventarioBLL.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using BRAMSELU.llamadoinventario.DAL;
+
+namespace BRAMSELU.llamadoinventario.BLL
+{
+    public class llamadoinventarioBLL
+    {
+        private llamadoinventarioDAL dal = new llamadoinventarioDAL();
+
+        public DataTable Listar()
+        {
+            return dal.Listar();
+        }
+    }
+}
---
+++ llamadoinventario/llamadoinventarioDAL.cs
@@ -0,0 +1,18 @@
+﻿using System.Data;
+
+namespace BRAMSELU.llamadoinventario.DAL
+{
+    public class llamadoinventarioDAL
+    {
+        public DataTable Listar()
+        {
+       
+            Conexion conexionBD = new Conexion();
+
+            string query = "SELECT IdProducto, NombreProducto, Marca, Categoria, Precio, Stock, FechaRegistro, Imagen, IdCategoria FROM Productos";
+
+           
+            return conexionBD.EjecutarConsultaDataTable(query);
+        }
+    }
+}
