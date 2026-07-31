5934e69c2043998293e561f0c7ac9b34399a49be
naomy1237rhjhfgr <lagosb813@gmail.com>
2026-07-30 20:04:01 -0600

    jhsdjhdsnxbsbsdh

---
+++ reporteproductos/ReporteProductosVendidosBLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+
+namespace BRAMSELU.reporteproductos
+{
+    public class ReporteProductosVendidosBLL
+    {
+        private ReporteProductosVendidosDAL objDAL = new ReporteProductosVendidosDAL();
+
+        public DataTable ObtenerProductosMasVendidos(int topCantidad)
+        {
+           
+            if (topCantidad <= 0)
+            {
+                topCantidad = 10;
+            }
+
+            return objDAL.ObtenerProductosMasVendidos(topCantidad);
+        }
+    }
+}
