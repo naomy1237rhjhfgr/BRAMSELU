f8f5931fa3e03007a543667c87cc0d84ab3c9716
naomy1237rhjhfgr <lagosb813@gmail.com>
2026-07-30 19:36:54 -0600

    jhcdhjsdhsdnjdjdsjhd

---
+++ reportecaja/CajaReporteModelo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BRAMSELU.reportecaja
+{
+    public class CajaReporteModelo
+    {
+        public int IdCaja { get; set; }
+        public DateTime FechaApertura { get; set; }
+        public DateTime FechaCierre { get; set; }
+        public decimal MontoInicial { get; set; }
+        public decimal TotalVentasEfectivo { get; set; }
+        public decimal MontoFinal { get; set; }
+        public string Estado { get; set; }
+        public string UsuarioApertura { get; set; }
+        public decimal TotalCompras { get; set; }
+    }
+}
---
+++ reportecaja/ReporteCajaDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+
+namespace BRAMSELU.reportecaja
+{
+    public class ReporteCajaDAL
+    {
+        private Conexion conexionObj = new Conexion();
+
+        public DataTable ObtenerArqueoCajaPorFecha(DateTime fechaInicio, DateTime fechaFin)
+        {
+            string query = $"SELECT IdCaja, FechaApertura, FechaCierre, MontoInicial, TotalVentasEfectivo, MontoFinal, Estado, UsuarioApertura, TotalCompras " +
+                           $"FROM Cajas " +
+                           $"WHERE FechaApertura >= '{fechaInicio:yyyy-MM-dd 00:00:00}' AND FechaApertura <= '{fechaFin:yyyy-MM-dd 23:59:59}' " +
+                           $"ORDER BY FechaApertura DESC";
+
+            return conexionObj.EjecutarConsultaDataTable(query);
+        }
+    }
+}
